810ff93bd336989ab1058bd7b9dd648694dcbf27
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-06-25 21:51:59 +0530

    Remove Scala stuff, restructure to standard convention

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/halcat0x15a/cont"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :source-paths ["src/main/clojure"]
-  :test-paths ["src/test/clojure"])
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :source-paths ["src"]
+  :test-paths ["test"])
